1b55e97ca9df8e7b8fbededb4f9e4ee1f655eda3
ribmarciojr <marciojunior2109@gmail.com>
2025-05-04 12:43:21 -0300

    build: basic spring project

---
+++ buzufba/src/main/java/br/com/condosnap/buzufba/BuzufbaApplication.java
@@ -0,0 +1,13 @@
+package br.com.condosnap.buzufba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuzufbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuzufbaApplication.class, args);
+	}
+
+}
